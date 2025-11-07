faa7f3c61673b2e1af845a71fe4a61b57c2797e7
Afreen Begum <afreenbegumaffu0@gmail.com>
2025-11-07 12:55:37 +0530

    added FridgeSingle.jsx and FurnitureSingle.jsx and updated css

---
+++ e-cart/src/singles/FridgeSingle.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { fridgeData } from '../components/data/fridge'
+import { useParams } from 'react-router-dom'
+import Navbar from '../components/Navbar'
+
+const FridgeSingle = () => {
+    const {id}=useParams()
+    const product=fridgeData.find(item=>item.id===id)
+  return (
+    <>
+    <Navbar/>
+    <div className="singleProduct">
+        <div className="singleImage">
+            <img src={product.image} alt="" />
+        </div>
+        <div className="productDetails">
+            <div className="singleModel">
+                <h3>{product.model}
+                </h3>
+            </div>
+            <div className="singlePrice">
+                <h3>{product.price}</h3>
+            </div>
+            <div className="singleDesc">
+                 <h3>{product.description}</h3>
+            </div>
+            <div className="singleModel">
+                <h3>{product.company}
+                </h3>
+                </div>
+            <button>Add to Cart</button>
+            <button>Buy</button>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default FridgeSingle
